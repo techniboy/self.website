ab6cced6bdde97c39c786535eece80ae7857fac9
Surya Prasad <techniboy.h2o@gmail.com>
2023-01-04 20:05:06 +0530

    add tailwindcss

--- src/components/IntroTextWrapper/IntroTextWrapper.component.tsx
+++ src/components/IntroTextWrapper/IntroTextWrapper.component.tsx
@@ -4,7 +4,7 @@ export const IntroTextWrapper: FC<{}> = ({}) => {
     return (
         <div className="intro-text-wrapper" style={{ willChange: 'transform', transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)', transformStyle: 'preserve-3d' }}>
         <div className="intro-text-line" style={{ opacity: 1 }}>
-          <h1 className="intro-text-word text-super">SURYA</h1>
+          <h1 className="text-3xl font-bold underline">SURYA</h1>
           <h1 className="intro-text-word text-super">PRASAD</h1>
         </div>
         <div className="intro-text-line" style={{ opacity: 1 }}>
